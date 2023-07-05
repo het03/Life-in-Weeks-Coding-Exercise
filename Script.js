eff3fd92a5fa07f32736b41c0293829a9cf4008c
het03 <het36100@gmail.com>
2023-07-05 16:55:43 +0530

    final commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+        var months;
+        var weeks;
+        var days;
+        
+        months = (90 - age)*12;
+        weeks = (90 - age)*52;
+        days = (90 - age)*365;
+        
+        console.log("You" + " have " + days + " days" + ", " + weeks + " weeks" + "," + " and " + months + " months" + " left" + ".");
+    /*************Don't change the code below**********/
+    }
